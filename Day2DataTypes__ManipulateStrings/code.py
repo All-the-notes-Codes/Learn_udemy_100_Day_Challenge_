f8bfa32ace30fdc4c414e4bbb37eaa4a82d4fef5
All-the-notes-Codes <consultant.Jaskaran@gmail.com>
2025-03-11 15:42:36 -0400

    Add files via upload

---
+++ Day2DataTypes__ManipulateStrings/code.py
@@ -0,0 +1,23 @@
+
+# String : 
+# Subscripting using Index
+"Hello"[1]
+"Karan"[-1] #'-1' will print the last value. 
+"Jason"[-2] # '2' will print the 2nd last value. 
+
+print("123" + "456") # will concatinate. 
+
+# Integer
+print(20+40)
+
+# Large Integers : 
+# Easy to read large number
+print(123_456_789)
+
+# Float
+print(3.14321)
+
+# Boolean
+print(True)
+print(False)
+
---
+++ Day2DataTypes__ManipulateStrings/notes.py
@@ -0,0 +1,3 @@
+# print and 'len'
+# 'len' function only accept 'string', bytes, tuple, list or range or a collection as argument. 
+print(len("Jaskaran Singh")) 
