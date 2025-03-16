abf052bcb0acad9e7fa8c56728d7f8e5012aa0a6
All-the-notes-Codes <consultant.Jaskaran@gmail.com>
2025-03-16 19:37:10 -0400

    Update code.py

---
+++ Day6_Functions_Karel/code.py
@@ -0,0 +1,49 @@
+
+# ------------------------"Functions":
+# Functions are the blocks of code which are used to perform a specific task.
+# Function overloading doe not work in python. You can use the same function name with different parameters by providing defa  ult values to the parameters. 
+
+
+# To Make your own function, you have to use the "def" keyword.
+def my_function():
+  print("Hello from my_function") # the code inside the function should be indented.
+  print("Bye from my_function")
+
+# To call the function, you have to use the function name.
+my_function() # Function has to be defined first before calling it.
+
+
+# Indention: Indention is the space at the beginning of a code line.
+# there should be no space at the beginning of a code line.
+
+# It's a good practice to use 4 spaces for indentation.
+# example: 
+#....print("This will be printed, because it's indented ( with 4 spaces ."))
+
+
+
+# While Loop: Loops through a block of code as long as a specified condition is true.
+# Syntax:
+'''
+while something_is_true:
+  # Do this repeatedly. 
+''' 
+
+n = 1
+while n < 10 :
+  print(n)
+  n += 1 
+
+
+# Use 'not' : This is equal to '!='
+# Example:
+'''
+while not 1  > 2 : 
+    print("1 is not greater than 2") # This will be printed 
+'''
+# 'not' can be used with 'if' and 'while' statements.
+
+
+# Practised Karel
+# : https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
